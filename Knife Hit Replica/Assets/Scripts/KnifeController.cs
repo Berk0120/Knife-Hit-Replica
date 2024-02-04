50fd5457c7edab404159f545081d3ab9c1f0eef9
Berk0120 <berkacici658@gmail.com>
2024-02-04 17:20:32 +0300

    Completed

---
+++ Knife Hit Replica/Assets/Scripts/KnifeController.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KnifeController : MonoBehaviour
+{
+    
+    private KnifeManager knifeManager;
+    private Rigidbody2D knifeRigidBody;
+
+    [SerializeField] private float moveSpeed;
+
+    private bool canShoot;
+    void Start()
+    {
+        GetComponentValues();
+    }
+
+   
+    void Update()
+    {
+        HandleShootInput();
+    }
+
+   
+
+
+    private void FixedUpdate()
+    {
+        Shoot();
+    }
+
+    private void HandleShootInput()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            canShoot = true;
+            knifeManager.SetDisabledIconColour();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Circle"))
+        {
+
+            knifeManager.SetActiveKnife();
+           
+            canShoot = false;
+            knifeRigidBody.isKinematic = true;
+            transform.SetParent(other.gameObject.transform);
+        }
+
+
+        if (other.gameObject.CompareTag("Knife"))
+        {
+            SceneManager.LoadScene(0);
+        }
+
+
+    }
+
+    private void Shoot()
+    {
+        if (canShoot)
+        {
+            knifeRigidBody.AddForce(Vector2.up * moveSpeed * Time.fixedDeltaTime);
+        }
+    }
+
+    private void GetComponentValues()
+    {
+        knifeRigidBody = GetComponent<Rigidbody2D>();
+        knifeManager = GameObject.FindObjectOfType<KnifeManager>(); 
+    }
+}
